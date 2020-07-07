e0112050af64c8be53514d4f0b48523d9676a48a
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-07-07 14:44:30 +0100

    Running lint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    'eslint:recommended',
-  ],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-var': 'error',
-    'indent': ['error', 'tab'],
-    'semi': ['error', 'never'],
-    'quotes': ['error', 'single'],
-    'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
-    'prefer-const': ['error']
-  },
-};
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		'plugin:vue/essential',
+		'eslint:recommended',
+	],
+	parserOptions: {
+		parser: 'babel-eslint',
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-var': 'error',
+		'indent': ['error', 'tab'],
+		'semi': ['error', 'never'],
+		'quotes': ['error', 'single'],
+		'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+		'prefer-const': ['error']
+	},
+}
