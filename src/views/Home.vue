7b6886b5f2bc9a1e68eae8e05ff275afe581c7b7
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-07-08 02:52:11 +0100

    Added home service components and assets

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div>
+		<Services />
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Services from '@/components/home/Services.vue'
 
 export default {
 	name: 'Home',
 	components: {
-		HelloWorld,
+		Services
 	},
 }
 </script>
