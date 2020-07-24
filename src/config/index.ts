81e91be8a5371a539545e6a3c253e94083768c7c
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-07-24 13:28:17 +0100

    Fixed config folder and commented out service worker

---
+++ src/config/index.ts
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import VueMeta from 'vue-meta'
+import CompositionApi from '@vue/composition-api'
+import VueJsModal from 'vue-js-modal'
+
+import '@/config/registerServiceWorker'
+import 'bootstrap'
+import '@/assets/style/index.scss'
+
+import RouteName from '@/components/application/RouteName.vue'
+import Loading from '@/components/application/Loading.vue'
+import AudioPlayer from '@/components/application/AudioPlayer.vue'
+
+Vue.use(VueMeta, { keyName: 'meta', refreshOnceOnNavigation: true })
+Vue.use(CompositionApi)
+Vue.use(VueJsModal, { dialog: true, dynamicDefaults: {
+	dynamic: true, width: '90%', minWidth: 600, maxWidth: 900,
+	height: 'auto', adaptive: false, scrollable: true, focusTrap: true
+}})
+
+Vue.component('RouteName', RouteName)
+Vue.component('Loading', Loading)
+Vue.component('AudioPlayer', AudioPlayer)
+
+export const closeNavbar = () => {
+	const collapse = document.getElementById('navbar')
+	collapse ? collapse.classList.remove('show') : null
+}
