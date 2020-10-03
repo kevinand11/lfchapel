5ec02ae4679f9b24591239225c4b9d971883a297
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-10-04 00:45:41 +0100

    Newsletter subscription

--- src/application/usecases/useFunctions.ts
+++
@@ -1,5 +0,0 @@
-import { FunctionsService } from '@modules/core/services/firebase'
-
-export const subscribeToMailingList = async (email: string) => {
-	return await FunctionsService.call('subscribeToMailingList', { email })
-}
