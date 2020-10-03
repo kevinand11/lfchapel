633811fa085d56dc6d34e80c2a8efc1868e6697e
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-10-03 20:02:08 +0100

    Fetching current user on load page

--- src/application/store/modules/users.ts
+++ src/application/store/modules/users.ts
@@ -16,32 +16,32 @@ const getters = {
 }
 
 const mutations = {
-	setId: (state, id) => state.id = id,
+	setId: (state, id) => {
+		state.id = id
+		if(id) window.localStorage.setItem('user_id', id)
+		else window.localStorage.removeItem('user_id')
+	},
 	setProfileListener: (state, listener) => {
 		state.profileListener()
 		state.profileListener = listener
 	},
-	setUser: (state, user) => state.user = user
+	setUser: (state, user) => {
+		state.user = user
+		if(user) window.localStorage.setItem('user', JSON.stringify(user))
+		else window.localStorage.removeItem('user')
+	}
 }
 
 const actions = {
 	setId: async({ commit }, id) => {
 		commit('setId', id)
-		commit('setUser', null)
 		await Store.dispatch('closeProfileListener')
 		if(id){
 			const user = await FindUser.call(id)
 			commit('setUser', user ?? null)
-			if(user){
-				commit('setUser', user)
-				window.localStorage.setItem('user', JSON.stringify(user))
-			}else commit('setId', null)
-			window.localStorage.setItem('user_id', id)
-		}else{
-			window.localStorage.removeItem('user')
-			window.localStorage.removeItem('user_id')
-		}
-
+			if(user) commit('setUser', user)
+			else commit('setId', null)
+		}else commit('setUser', null)
 	},
 	closeProfileListener: ({ commit }) => commit('setProfileListener', () => {}),
 }
