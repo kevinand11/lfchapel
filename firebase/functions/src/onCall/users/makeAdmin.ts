39390953d5a9c9c69c53a9889eb86dafa91bc9ca
Kevin Izuchukwu <kevinfizu@gmail.com>
2021-01-24 02:17:10 +0100

    removing dev auth

--- firebase/functions/src/onCall/users/makeAdmin.ts
+++ firebase/functions/src/onCall/users/makeAdmin.ts
@@ -1,19 +1,18 @@
 import * as functions from 'firebase-functions'
 import * as admin from'firebase-admin'
-import { isProduction } from '../../helpers/environment'
 
 export const makeAdmin = functions.https.onCall(async ({ id }, context) => {
-	if (isProduction() && !context.auth) {
+	if (!context.auth) {
 		throw new functions.https.HttpsError('unauthenticated', 'Only authenticated users can manage user accounts')
 	}
-	if (isProduction() && !context.auth?.token.isAdmin) {
+	if (!context.auth?.token.isAdmin) {
 		throw new functions.https.HttpsError('failed-precondition', 'Only admins can manage user accounts')
 	}
-	if (isProduction() && context.auth?.uid === id) {
+	if (context.auth?.uid === id) {
 		throw new functions.https.HttpsError('failed-precondition', 'You cannot change your own role')
 	}
 	try{
-		if (isProduction()) await admin.auth().setCustomUserClaims(id, { isAdmin: true })
+		await admin.auth().setCustomUserClaims(id, { isAdmin: true })
 		await admin
 			.firestore()
 			.collection('users')
